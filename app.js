ba84ce2493fbb8b740652318edae764c85de7fad
ChyaBoiNik <nikolas.gephart@gmail.com>
2021-06-30 15:41:58 -0600

    createing llogic

--- app.js
+++ app.js
@@ -1,5 +1,46 @@
+let options = {
+  rock: {
+    displayMessage: '',
+    winsAgainst: 'scissors',
+    losesTo: 'paper'
+  },
+  paper: {
+    displayMessage: '',
+    winsAgainst: 'rock',
+    losesTo: 'scissors'
 
-function play(playerChoice) {
-  let choice = playerChoice
-
+  },
+  scissors: {
+    displayMessage: '',
+    winsAgainst: 'paper',
+    losesTo: 'rock'
+  }
 }
+
+
+function rockWin() {
+  console.log('moo')
+  document.getElementById("speak-output").innerText = "The Cow says Moo."
+
+  function paperWin() {
+    console.log('moo')
+    document.getElementById("speak-output").innerText = "The Cow says Moo."
+
+    function scissorwin() {
+      console.log('moo')
+      document.getElementById("speak-output").innerText = "The Cow says Moo."
+
+
+
+
+      function play(playerChoice) {
+        let choice = playerChoice
+
+
+
+
+
+
+
+
+      }
