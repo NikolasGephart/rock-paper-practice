804c2995920d2717ded42c349440af6d4adc8a07
ChyaBoiNik <nikolas.gephart@gmail.com>
2021-06-30 15:42:46 -0600

    forgot ending brackets....oops

--- app.js
+++ app.js
@@ -21,26 +21,28 @@ let options = {
 function rockWin() {
   console.log('moo')
   document.getElementById("speak-output").innerText = "The Cow says Moo."
+}
 
-  function paperWin() {
-    console.log('moo')
-    document.getElementById("speak-output").innerText = "The Cow says Moo."
+function paperWin() {
+  console.log('moo')
+  document.getElementById("speak-output").innerText = "The Cow says Moo."
+}
 
-    function scissorwin() {
-      console.log('moo')
-      document.getElementById("speak-output").innerText = "The Cow says Moo."
-
-
-
-
-      function play(playerChoice) {
-        let choice = playerChoice
+function scissorwin() {
+  console.log('moo')
+  document.getElementById("speak-output").innerText = "The Cow says Moo."
+}
 
 
 
-
+function play(playerChoice) {
+  let choice = playerChoice
 
 
 
 
-      }
+
+
+
+
+}
